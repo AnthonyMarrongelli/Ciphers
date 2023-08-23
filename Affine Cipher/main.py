8e7b276921c70968567e5c837544d451a6b6c041
Anthony <an769634@knights.ucf.edu>
2023-08-23 17:10:31 -0400

    Affine Cipher Collection

---
+++ Affine Cipher/main.py
@@ -0,0 +1,60 @@
+import sys
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'     #Our alphabet we will use for indexing
+
+#Arguments given by user
+input_file = str(sys.argv[1])
+key1 = int(sys.argv[2])
+key2 = int(sys.argv[3])
+
+
+inputArray = []     #Array that will hold our encrypted message
+inputText = open(input_file, 'r', encoding = 'utf-8')  #Opening of intake file
+
+
+#Reading encrypted message from a file into an array
+try:
+    i = 0
+    while(1):
+        char = inputText.read(1)
+        
+        #break when we reach end of file
+        if not char:
+            break
+        
+        #excluding spaces
+        if char == ' ':
+            continue
+        
+        inputArray.append(char.lower())
+        i+=1
+finally:
+        if not ''.join(inputArray).isalpha: raise Exception("Message to be encrypted has illegal characters.")
+        inputText.close()
+
+#function for encrypting a message with a given key pair
+def encrypt(inputArray, key1, key2):
+    encryptedArray = []
+    
+    for item in inputArray:
+        encryptedArray.append(alphabet[(key1*(alphabet.index(item)) + key2) % 26])
+    
+    return encryptedArray
+
+#function for decrypting a message with a given key pair
+def decrypt(encryptedArray, key1, key2):
+    decryptedArray = []
+    
+    for item in encryptedArray:
+        decryptedArray.append('')
+    
+    return decryptedArray
+
+
+output = open('output.txt', 'w', encoding = 'utf-8')    #Opening output file to write encryption/decryption
+
+encryptedMessage = encrypt(inputArray, key1, key2)
+
+output.write('Encrypted Message: \n' + ''.join(encryptedMessage) + '\n')    #Displaying after encrypted
+output.write('After Decryption: \n' + ''.join(decrypt(encryptedMessage, key1, key2)) + '\n')   #Displaying after encrypted was decrypted
+    
